58babfbad5685a5c879617e9fe9d5400ad3ca236
Zachary <759740844@qq.com>
2016-09-09 10:08:51 +0800

    add "NSLayoutConstraintDemo"

---
+++ NSLayoutConstraintDemo/NSLayoutConstraintDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSLayoutConstraintDemo
+//
+//  Created by Zachary on 16/9/8.
+//  Copyright © 2016年 Zachary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NSLayoutConstraintDemo/NSLayoutConstraintDemo/ViewController.m
@@ -0,0 +1,119 @@
+//
+//  ViewController.m
+//  NSLayoutConstraintDemo
+//
+//  Created by Zachary on 16/9/8.
+//  Copyright © 2016年 Zachary. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "Masonry.h"
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    //创建view
+    UIView *view = [[UIView alloc] init]; //这里不需要设置frame
+    view.backgroundColor = [UIColor brownColor];
+    view.translatesAutoresizingMaskIntoConstraints = NO; //要实现自动布局，必须把该属性设置为NO
+    [self.view addSubview:view];
+    
+    //添加约束
+    [self.view addConstraint:
+     [NSLayoutConstraint constraintWithItem:view
+                                  attribute:NSLayoutAttributeLeft
+                                  relatedBy:NSLayoutRelationEqual
+                                     toItem:self.view
+                                  attribute:NSLayoutAttributeLeft
+                                 multiplier:1
+                                   constant:20]];
+    [self.view addConstraint:
+     [NSLayoutConstraint constraintWithItem:view
+                                  attribute:NSLayoutAttributeRight
+                                  relatedBy:NSLayoutRelationEqual
+                                     toItem:self.view
+                                  attribute:NSLayoutAttributeRight
+                                 multiplier:1
+                                   constant:-10]];
+    [self.view addConstraint:
+     [NSLayoutConstraint constraintWithItem:view
+                                  attribute:NSLayoutAttributeTop
+                                  relatedBy:NSLayoutRelationEqual
+                                     toItem:self.view
+                                  attribute:NSLayoutAttributeTop
+                                 multiplier:1
+                                   constant:30]];
+    [self.view addConstraint:
+     [NSLayoutConstraint constraintWithItem:view
+                                  attribute:NSLayoutAttributeBottom
+                                  relatedBy:NSLayoutRelationEqual
+                                     toItem:self.view
+                                  attribute:NSLayoutAttributeBottom
+                                 multiplier:1
+                                   constant:-20]];
+    
+    //使用Masonry添加约束
+    UIView *sv = self.view;
+    
+    UIScrollView *scrollView = [UIScrollView new];
+    scrollView.backgroundColor = [UIColor whiteColor];
+    [sv addSubview:scrollView];
+    [scrollView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(sv).with.insets(UIEdgeInsetsMake(5,5,5,5));
+    }];
+    
+    UIView *container = [UIView new];
+    [scrollView addSubview:container];
+    [container mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(scrollView);
+        make.width.equalTo(scrollView);
+    }];
+    
+    int count = 10;
+    
+    UIView *lastView = nil;
+    
+    for ( int i = 1 ; i <= count ; ++i )
+    {
+        UIView *subv = [UIView new];
+        [container addSubview:subv];
+        subv.backgroundColor = [UIColor colorWithHue:( arc4random() % 256 / 256.0 )
+                                          saturation:( arc4random() % 128 / 256.0 ) + 0.5
+                                          brightness:( arc4random() % 128 / 256.0 ) + 0.5
+                                               alpha:1];
+        
+        [subv mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.left.and.right.equalTo(container);
+            make.height.mas_equalTo(@(20*i));
+            
+            if ( lastView )
+            {
+                make.top.mas_equalTo(lastView.mas_bottom);
+            }
+            else
+            {
+                make.top.mas_equalTo(container.mas_top);
+            }
+        }];
+        
+        lastView = subv;
+    }
+    
+    
+    [container mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.bottom.equalTo(lastView.mas_bottom);
+    }];
+}
+
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
