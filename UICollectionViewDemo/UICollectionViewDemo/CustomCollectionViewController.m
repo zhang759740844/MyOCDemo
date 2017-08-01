d97687b7f4b03648c30f53fdfcde44f3eb051ac0
Zachary Zhang <759740844@qq.com>
2017-08-01 17:34:16 +0800

    change

--- UICollectionViewDemo/UICollectionViewDemo/CustomCollectionViewController.m
+++ UICollectionViewDemo/UICollectionViewDemo/CustomCollectionViewController.m
@@ -45,7 +45,12 @@ static NSString * const reuseIdentifier = @"Cell";
     self.layoutChangeSegmentedControl = [[UISegmentedControl alloc] initWithItems:@[@"Circle", @"Flow"]];
     self.layoutChangeSegmentedControl.selectedSegmentIndex = 0;
     [self.layoutChangeSegmentedControl addTarget:self action:@selector(layoutChangeSegmentedControlDidChangeValue:) forControlEvents:UIControlEventValueChanged];
-    self.navigationItem.titleView = self.layoutChangeSegmentedControl; 
+    self.navigationItem.titleView = self.layoutChangeSegmentedControl;
+    
+    
+    //collectionView不像tableView可以设置setEditing属性切换是否能够编辑，需要声明长按手势在 viewDidload 方法中
+    UILongPressGestureRecognizer *longGesture = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(handlelongGesture:)];
+    [self.collectionView addGestureRecognizer:longGesture];
 }
 
 - (void)addItem {
@@ -67,9 +72,14 @@ static NSString * const reuseIdentifier = @"Cell";
     if (self.collectionView.collectionViewLayout == _circleLayout) {
         [_flowLayout invalidateLayout];
         [self.collectionView setCollectionViewLayout:_flowLayout animated:YES];
+        [self.collectionView performBatchUpdates:^{
+            self.cellCount--;
+            [self.collectionView deleteItemsAtIndexPaths:@[[NSIndexPath indexPathForItem:0 inSection:0]]];
+        } completion:nil];
     } else {
         [_circleLayout invalidateLayout];
         [self.collectionView setCollectionViewLayout:_circleLayout animated:YES];
+        [self.collectionView reloadData];
     }
 }
 
@@ -87,6 +97,65 @@ static NSString * const reuseIdentifier = @"Cell";
     return cell;
 }
 
+#pragma mark - 高亮
+// 允许选中时，高亮
+-(BOOL)collectionView:(UICollectionView *)collectionView shouldHighlightItemAtIndexPath:(NSIndexPath *)indexPath{
+    return YES;
+}
+// 高亮完成后回调
+// 放大缩小效果
+-(void)collectionView:(UICollectionView *)collectionView didHighlightItemAtIndexPath:(NSIndexPath *)indexPath{
+    UICollectionViewCell *selectedCell = [collectionView cellForItemAtIndexPath:indexPath];
+    [UIView animateWithDuration:1 animations:^{
+        selectedCell.transform = CGAffineTransformMakeScale(2.0f, 2.0f);
+    }];
+}
+// 由高亮转成非高亮完成时的回调
+-(void)collectionView:(UICollectionView *)collectionView didUnhighlightItemAtIndexPath:(NSIndexPath *)indexPath{
+    UICollectionViewCell *selectedCell = [collectionView cellForItemAtIndexPath:indexPath];
+    [UIView animateWithDuration:1 animations:^{
+        selectedCell.transform = CGAffineTransformMakeScale(1.0f, 1.0f);
+    }];
+}
+
+#pragma mark - 移动
+//返回YES允许其item移动
+- (BOOL)collectionView:(UICollectionView *)collectionView canMoveItemAtIndexPath:(NSIndexPath *)indexPath{
+    return YES;
+}
+
+//移动item时回调
+- (void)collectionView:(UICollectionView *)collectionView moveItemAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath*)destinationIndexPath {
+}
+
+
+//再实现手势操作
+- (void)handlelongGesture:(UILongPressGestureRecognizer *)longGesture {
+    //判断手势状态
+    switch (longGesture.state) {
+        case UIGestureRecognizerStateBegan:{
+            //判断手势落点位置是否在路径上
+            NSIndexPath *indexPath = [self.collectionView indexPathForItemAtPoint:[longGesture locationInView:self.collectionView]];
+            if (indexPath == nil) {
+                break;
+            }
+            //在路径上则开始移动该路径上的cell
+            [self.collectionView beginInteractiveMovementForItemAtIndexPath:indexPath];
+        }
+            break;
+        case UIGestureRecognizerStateChanged:
+            //移动过程当中随时更新cell位置
+            [self.collectionView updateInteractiveMovementTargetPosition:[longGesture locationInView:self.collectionView]];
+            break;
+        case UIGestureRecognizerStateEnded:
+            //移动结束后关闭cell移动
+            [self.collectionView endInteractiveMovement];
+            break;
+        default:
+            [self.collectionView cancelInteractiveMovement];
+            break;
+    }
+}
 
 
 @end
